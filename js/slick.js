31dbe2b8aaf574e1b6e5e84b19dd4c3efb535133
Sheffchenk0 <bladdem2017@gmail.com>
2020-04-20 22:58:47 +0300

    e

---
+++ js/slick.js
@@ -0,0 +1,21 @@
+
+$('.products-content').slick({
+    arrows:true,
+    slidesToShow:3,
+    autoplay:true,
+    speed:400,
+    autoplaySpeed:4000,
+    slidesToScroll: 1,
+});
+
+$('.shirt-div').slick({
+    arrows:false,
+    slidesToShow:1,
+    dots: true
+});
+
+$('.second-shirt').slick({
+    arrows:false,
+    slidesToShow:1,
+    dots: true
+});
